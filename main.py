50fc96b4278526aa496b665f6c97aab8e40dd873
temirkanmo <96943031+temirkanmo@users.noreply.github.com>
2024-12-07 23:05:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from blockchain import Blockchain, Block
+from time import time
+
+
+def main():
+    # Blockchain is initialized
+    bc = Blockchain()
+
+    # Alice transferred 100 tokens to Bob
+    bc.addBlock(Block(str(int(time())), ({"from": "Alice", "to": "Bob", "amount": 100})))
+    
+    # Bob transferred 100 tokens back to Alice
+    bc.addBlock(Block(str(int(time())), ({"from": "Bob", "to": "Alice", "amount": 100})))
+
+    # Print blockahin
+    print(bc)
+
+
+if __name__ == "__main__":
+    main()
